cb188c3d09880d749548a3ec598c452255657470
gueye <alassane.gueye@etu.univ-lehavre.fr>
2023-10-08 19:18:22 +0200

    routes pour la page d'accueil

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  res.render('index', { user : req.user,title:'Home Page',currentPage:'annonces' });
 });
 
+router.get('/error-page', async (req, res, next) => {
+  res.render('error', { user : req.user,title:'Home Page',errorMessage: 'Annonce non trouvée' });
+});
+
+
 module.exports = router;
+
